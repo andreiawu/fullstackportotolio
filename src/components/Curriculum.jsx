199ac249b8b16f10961f5e4fa982e945f0b8fdae
andreia <andreia.12345@hotmail.com>
2024-06-23 22:46:46 +0100

    cv button working, downloads pdf file

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 export default function Curriculum() {
+
+  const handleDownload = () => {
+    const url = process.env.PUBLIC_URL + '/CV_AndreiaWu_.pdf'; 
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'CV_AndreiaWu_.pdf');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer'); 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="cv">
       <button
-        className="download-cv">
+        className="download-cv"
+        onClick={handleDownload}
+      >
         <div className="center-btn-cv">
           <img src="/images/download-cv.png" alt="download-icon" className="download-icon" />
           <span className="cv-word">CV</span>
