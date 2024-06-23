4444e7d9598480298beb0128cf4f7bccf0000d7b
andreia <andreia.12345@hotmail.com>
2024-06-23 14:18:46 +0100

    hire button linked

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import AboutMeCircles from "./AboutMeCircles"
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 export default function AboutMe() {
   return (
@@ -13,13 +14,15 @@ export default function AboutMe() {
         <p className="about-p">
           👋 Hey! I'm Andreia. I come from a fashion background, but I've decided to change my path and dive into the world of technology. I'm passionate about design and functionality, and I absolutely love front-end development. Anything challenging really gets me excited! But what truly drives and inspires me is seeing the final result of my creations.
         </p>
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            transition: { duration: 0.2 },
-          }}
-          whileTap={{ scale: 0.9 }}
-          className="hire-me-btn">HIRE ME</motion.button>
+        <Link to='/contact'>
+          <motion.button
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="hire-me-btn">HIRE ME</motion.button>
+        </Link>
       </div>
     </div>
   )
