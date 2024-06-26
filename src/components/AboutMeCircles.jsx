f5860f17fb605c09303938b986f8f2557d1db7f4
andreia <andreia.12345@hotmail.com>
2024-06-26 14:29:57 +0100

    ilustration

--- src/components/AboutMeCircles.jsx
+++ src/components/AboutMeCircles.jsx
@@ -5,6 +5,7 @@ export default function AboutMeCircles() {
     <div className="circles">
       <img src="/images/circle.png" alt="circle" className="circle" />
       <img src="images/blur-circle.png" alt="blur" className="blur" />
+      <img src='/images/about-ilustration.png' alt='about-ilustration' className='about-ilustration'></img>
     </div>
   )
 }
