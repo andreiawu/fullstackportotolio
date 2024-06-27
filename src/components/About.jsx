8bfb9330c20c24569c133a098ae3a355d49204f7
andreia <andreia.12345@hotmail.com>
2024-06-27 16:12:05 +0100

    fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,17 @@ import Skills from './Skills';
 import Journey from './Journey';
 import Hobbies from './Hobbies';
 import Scroll2 from './Scroll2';
-import Fade from 'react-reveal/Fade'
+// import Fade from 'react-reveal/Fade'
 
 export default function About() {
   return (
     <div className='about-wrapper'>
-      <Fade top distance="10%" duration={1500}>
+      {/* <Fade top distance="10%" duration={1500}> */}
         <AboutMe />
         <Skills />
         <Journey />
         <Hobbies />
-      </Fade>
+      {/* </Fade> */}
       <Scroll2 />
     </div>
   )
